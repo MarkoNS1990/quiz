b80bb3979a5e676fb3207dee336cb3079392484f
Marko Zecevic <mzecevic@suitsupply.com>
2025-11-10 15:29:30 +0100

    Add 80 hard flag quiz questions (exotic and difficult countries)

---
+++ hard-flag-questions-80.sql
@@ -0,0 +1,99 @@
+-- 80 JAKO TEŠKIH pitanja o zastavama
+-- Male, egzotične i teško prepoznatljive zemlje
+
+INSERT INTO quiz_questions (question, answer, image_url, category, difficulty) VALUES
+
+-- AFRIKA (20 pitanja)
+('Koja je ovo zastava?', 'Burkina Faso', 'https://flagcdn.com/w320/bf.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Benin', 'https://flagcdn.com/w320/bj.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Bocvana', 'https://flagcdn.com/w320/bw.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Burundi', 'https://flagcdn.com/w320/bi.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Kamerun', 'https://flagcdn.com/w320/cm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Zelenortska Ostrva', 'https://flagcdn.com/w320/cv.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Dzibuti', 'https://flagcdn.com/w320/dj.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Ekvatorijalna Gvineja', 'https://flagcdn.com/w320/gq.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Eritreja', 'https://flagcdn.com/w320/er.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gabon', 'https://flagcdn.com/w320/ga.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gambija', 'https://flagcdn.com/w320/gm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gvineja', 'https://flagcdn.com/w320/gn.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gvineja-Bisao', 'https://flagcdn.com/w320/gw.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Lesoto', 'https://flagcdn.com/w320/ls.png', 'Zastave', 'teško'),
+('Koja je oto zastava?', 'Malavi', 'https://flagcdn.com/w320/mw.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Mauritanija', 'https://flagcdn.com/w320/mr.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Mozambik', 'https://flagcdn.com/w320/mz.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Niger', 'https://flagcdn.com/w320/ne.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Ruanda', 'https://flagcdn.com/w320/rw.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Sijera Leone', 'https://flagcdn.com/w320/sl.png', 'Zastave', 'teško'),
+
+-- AZIJA I PACIFIK (20 pitanja)
+('Koja je ovo zastava?', 'Laos', 'https://flagcdn.com/w320/la.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Kambodza', 'https://flagcdn.com/w320/kh.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Mjanmar', 'https://flagcdn.com/w320/mm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Banglades', 'https://flagcdn.com/w320/bd.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Butan', 'https://flagcdn.com/w320/bt.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Brunej', 'https://flagcdn.com/w320/bn.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Turkmenistan', 'https://flagcdn.com/w320/tm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Uzbekistan', 'https://flagcdn.com/w320/uz.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Kirgistan', 'https://flagcdn.com/w320/kg.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Tadzikistan', 'https://flagcdn.com/w320/tj.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Mongolija', 'https://flagcdn.com/w320/mn.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Papua Nova Gvineja', 'https://flagcdn.com/w320/pg.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Fidzi', 'https://flagcdn.com/w320/fj.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Samoa', 'https://flagcdn.com/w320/ws.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Tonga', 'https://flagcdn.com/w320/to.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Vanuatu', 'https://flagcdn.com/w320/vu.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Solomonska Ostrva', 'https://flagcdn.com/w320/sb.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Palau', 'https://flagcdn.com/w320/pw.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Mikronezija', 'https://flagcdn.com/w320/fm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Marsalska Ostrva', 'https://flagcdn.com/w320/mh.png', 'Zastave', 'teško'),
+
+-- SREDNJA I JUZNA AMERIKA + KARIBI (20 pitanja)
+('Koja je ovo zastava?', 'Belize', 'https://flagcdn.com/w320/bz.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gvatemala', 'https://flagcdn.com/w320/gt.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Honduras', 'https://flagcdn.com/w320/hn.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Nikaragva', 'https://flagcdn.com/w320/ni.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'El Salvador', 'https://flagcdn.com/w320/sv.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Kostarika', 'https://flagcdn.com/w320/cr.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Panama', 'https://flagcdn.com/w320/pa.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gvajana', 'https://flagcdn.com/w320/gy.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Surinam', 'https://flagcdn.com/w320/sr.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Paragvaj', 'https://flagcdn.com/w320/py.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Urugvaj', 'https://flagcdn.com/w320/uy.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Jamajka', 'https://flagcdn.com/w320/jm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Trinidad i Tobago', 'https://flagcdn.com/w320/tt.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Barbados', 'https://flagcdn.com/w320/bb.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Sveta Lucija', 'https://flagcdn.com/w320/lc.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Grenada', 'https://flagcdn.com/w320/gd.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Sveti Vincent i Grenadini', 'https://flagcdn.com/w320/vc.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Antigva i Barbuda', 'https://flagcdn.com/w320/ag.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Dominika', 'https://flagcdn.com/w320/dm.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Sveti Kristofor i Nevis', 'https://flagcdn.com/w320/kn.png', 'Zastave', 'teško'),
+
+-- BLISKI ISTOK I SEVERNA AFRIKA (10 pitanja)
+('Koja je ovo zastava?', 'Jemen', 'https://flagcdn.com/w320/ye.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Oman', 'https://flagcdn.com/w320/om.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Katar', 'https://flagcdn.com/w320/qa.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Bahrein', 'https://flagcdn.com/w320/bh.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Kuvajt', 'https://flagcdn.com/w320/kw.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Jordan', 'https://flagcdn.com/w320/jo.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Liban', 'https://flagcdn.com/w320/lb.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Sirija', 'https://flagcdn.com/w320/sy.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Libija', 'https://flagcdn.com/w320/ly.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Alžir', 'https://flagcdn.com/w320/dz.png', 'Zastave', 'teško'),
+
+-- OSTALE TEŠKE (10 pitanja)
+('Koja je ovo zastava?', 'Estonija', 'https://flagcdn.com/w320/ee.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Letonija', 'https://flagcdn.com/w320/lv.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Litvanija', 'https://flagcdn.com/w320/lt.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Moldavija', 'https://flagcdn.com/w320/md.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Belorusija', 'https://flagcdn.com/w320/by.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Gruzija', 'https://flagcdn.com/w320/ge.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Jermenija', 'https://flagcdn.com/w320/am.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Azerbejdzan', 'https://flagcdn.com/w320/az.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Kazahstan', 'https://flagcdn.com/w320/kz.png', 'Zastave', 'teško'),
+('Koja je ovo zastava?', 'Malta', 'https://flagcdn.com/w320/mt.png', 'Zastave', 'teško');
+
+-- Ukupno: 80 teških pitanja o zastavama
+-- Kategorije: Afrika, Azija, Pacifik, Karibi, Srednja Amerika, Bliski Istok, Baltik
+-- Sve zastave su male i teško prepoznatljive zemlje
+
